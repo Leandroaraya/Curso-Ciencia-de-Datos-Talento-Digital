625af637b89d2a32bacc1421123bef7eb0e5b1ac
Leandroaraya <araya.leandro@gmail.com>
2026-04-16 22:51:27 -0400

    funciones en python

---
+++ Modulo_02_Fundamentos_de_Python/04_Funciones_y_Modulos_Python/modulo1.py
@@ -0,0 +1,8 @@
+def cuadra(numero):
+    """esta funcion calcula el cuadrado de un numero"""
+    """#guardar en un Modulo la funcion"""
+    return numero**2
+
+def cuadrados(numero1,numero2):
+    """esta funcion calcula dos numeros cuadrados independ"""
+    return (numero1**2,numero2**2)
